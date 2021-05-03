be3e32555ab1af5497de8f0cefdf229453090c26
Jaqueline Lima <silvacirino.jaqueline@gmail.com>
2021-05-03 05:22:27 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,71 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Jorginho do SUS',
+                partido: 'ABC',
+                fotos:[
+                    {url: '10.jpg', legenda: 'Vereador'}
+                ]
+            },{
+                numero: '83541',
+                nome:'Bruno Fernando',
+                partido: 'ABC',
+                fotos:[
+                    {url: '20.png', legenda: 'Vereador'}
+                ]
+            },{
+                numero: '77222',
+                nome:'Beltrano da Silva',
+                partido:'DEFG',
+                fotos:[
+                    {url:'30.jpg', legenda: 'Vereador'}
+                ]
+            },{
+                numero: '98652',
+                nome:'Pedro Henrique',
+                partido: 'PLK',
+                foto:[
+                    {url: '40.jpg', legenda: 'Vereador'}
+                ]
+            },{
+                numero: '25657',
+                nome: 'Joao Grilo',
+                partido: 'TN',
+                foto:[
+                    {url: '50.jpg', legenda: 'Vereador'}
+                ]
+
+            }],
+        },
+        {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome:'Ciclano',
+                partido: 'ABC',
+                vice: 'Cizo',
+                fotos:[
+                    {url: 'ciclano.jpg', legenda: 'Prefeito'},
+                    {url: 'Cizo.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Tarziso',
+                partido: 'TN',
+                vice:'Felipi',
+                fotos:[
+                    {url:'60.jpg', legenda: 'Prefeito'},
+                    {url:'70.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+                    
+    }
+]
